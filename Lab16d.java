dd8495b2d10c110df9fb9a2386a8f829771cf339
connorlauff <connor@lauffenburgers.com>
2018-03-12 22:30:49 -0700

    Add files via upload

---
+++ Lab16d.java
@@ -0,0 +1,11 @@
+import static java.lang.System.*;
+
+public class Lab16
+{
+  public static void main( String args[] )
+  {
+    MadLib hop = new MadLib("H:/Unit10/Unit10-2016/Unit10-Assignments/Lab16d/story.dat");
+    
+    out.println(hop);
+  }
+}
