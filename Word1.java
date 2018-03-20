346a35da7129fd4ecb9b8f21ba7c74665ab6bb64
connorlauff <connor@lauffenburgers.com>
2018-03-19 23:28:42 -0700

    Unit 12

---
+++ Word1.java
@@ -0,0 +1,41 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import static java.lang.System.*;
+//Lab 18d
+public class Word1 implements Comparable<Word>
+{
+	private String word;
+	public Word1( String s )
+	{
+		word = s;
+	}
+
+	public int compareTo( Word rhs )
+	{
+		Word r = (Word)rhs;
+		if (r.toString().length() < word.length()) {
+			return 1;
+		}
+		else if (r.toString().length() > word.length()) {
+			return -1;
+		}
+		else if (r.toString().length() == word.length()) {
+			int ascii1 = r.toString().charAt(0);
+			int ascii2 = word.charAt(0);
+			if (ascii1 > ascii2)
+				return -1;
+		}
+		else {
+		  return 0;
+		}
+	}
+
+	public String toString()
+	{
+		return word;
+	}
+}
