a5aa4b91840c1abd4570a4fbc732b17e28d028d8
connorlauff <connor@lauffenburgers.com>
2018-03-26 09:30:02 -0700

    Unit 14 uncomplete

---
+++ Lab21g.java
@@ -0,0 +1,20 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import static java.lang.System.*;
+
+public class Lab21g
+{
+	public static void main(String args[])
+	{
+		//instantiate an AtCounter
+		AtCounter test = new AtCounter();
+		test.countAts(0, 0);
+		test.getAtCount();
+		test.toString();
+		//test the code
+	}
+}
